fc2ae0d9486ba8c9144b23636abd41490ffaa2c4
Sivasankari M <sivasankarim2006@gmail.com>
2025-06-17 00:40:42 +0530

    Delete complex_add.py

--- complex_add.py
+++
@@ -1,9 +0,0 @@
-# Define two complex numbers
-complex1 = complex(2, 3)  # 2 + 3j
-complex2 = complex(1, -4)  # 1 - 4j
-
-# Add the complex numbers
-result = complex1 + complex2
-
-# Display the result
-print(f"The sum of {complex1} and {complex2} is {result}")
